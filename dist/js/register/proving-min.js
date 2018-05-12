724d7d67b08d33b01322abd52c3bf58f21c5d72b
WoodenMan001 <347335621@qq.com>
2018-05-12 17:04:17 +0800

    delete dist

--- dist/js/register/proving-min.js
+++
@@ -1,1 +0,0 @@
-define(["../common/random-min","../common/cookieJar-min"],function(e,o){return{setCode:function(){let n=document.getElementById("code"),t="";for(var c=0;c<4;c++)t+=e.getCode();n.innerHTML=t,n.style.background=e.getColor(),o.setCookie("codeStr",t,1)},codeBtn:function(){let e=this;return document.querySelector("#code +a").onclick=function(){e.setCode()},this.codeStr}}});
--- dist/js/register/register-min.js
+++
@@ -1,1 +0,0 @@
-require(["dropDown-min","proving-min","form"],function(o,n,e){o.mouseover(),n.setCode(),n.codeBtn(),e.formValidation()});
