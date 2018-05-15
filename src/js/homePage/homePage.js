64324a2b23cf7fbf1ae4f08f5d9c5efdc81c9261
WoodenMan001 <347335621@qq.com>
2018-05-15 11:48:47 +0800

    homePage style

--- src/js/homePage/homePage.js
+++ src/js/homePage/homePage.js
@@ -2,11 +2,11 @@
 * @Author: WoodenMan001
 * @Date:   2018-05-14 11:56:24
 * @Last Modified by:   WoodenMan001
-* @Last Modified time: 2018-05-14 15:11:07
+* @Last Modified time: 2018-05-15 11:45:41
 */
-require(['init-min','../common/dropDown-min'], function (init,dd){
+require(['../common/jquery-min','init-min','../common/dropDown-min'], function ($,init,dd){
 
-	init.init();
+	init.init($);
 	//鼠标经过显示隐藏下拉框
 	dd.mouseover(["fa fa-angle-up","fa fa-angle-down"]);
 
--- src/js/homePage/init.js
+++ src/js/homePage/init.js
@@ -2,17 +2,18 @@
 * @Author: WoodenMan001
 * @Date:   2018-05-14 15:05:04
 * @Last Modified by:   WoodenMan001
-* @Last Modified time: 2018-05-14 15:21:34
+* @Last Modified time: 2018-05-15 11:47:03
 */
 define(['../common/common'],function(cm) {
 	return {
-		init: function() {
+		init: function($) {
 			//地址下拉框
-			let addArr = document.querySelector(".head_nav_address >.drop-down");
+			let addArr = $(".head_nav_address >.drop-down");
 			//收缩按钮
-			let oSelect = document.querySelector('.select');
-			let search_all_category  = document.getElementById('search_all_category');
+			let oSelect = $('.select');
 
+			let search_all_category  = $('#search_all_category');
+			console.log(addArr,oSelect,search_all_category)
 			this.selectfn(oSelect,search_all_category)
 		},
 		selectfn:function(onObj,obj) {
